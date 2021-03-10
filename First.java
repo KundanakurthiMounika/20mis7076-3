84f87280b08440c161388fa012d788f7dcc605a7
KundanakurthiMounika <79200349+KundanakurthiMounika@users.noreply.github.com>
2021-03-10 13:09:07 +0000

    Create First.java

---
+++ First.java
@@ -0,0 +1,9 @@
+public First{
+  public static void main(String ar[]){
+    int num1=100;
+    int num2=100,temp;
+    Sum s=new Sum();
+    temp=s.num1+num2;
+    System.out.println("The sum is +temp);
+                       }               
+                       }
