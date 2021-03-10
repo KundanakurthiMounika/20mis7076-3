595f1975683ae6e5773fdb4c0e793437ab327547
KundanakurthiMounika <79200349+KundanakurthiMounika@users.noreply.github.com>
2021-03-10 13:17:17 +0000

    Create Name.java

---
+++ Name.java
@@ -0,0 +1,8 @@
+public Name{
+  public static void main(String ar[]){
+    num1=120,num2=100,temp;
+    Subtract s= new Subtract();
+    temp=s.num1-num2;
+    System.out.println("The subtract is "+temp)
+  }
+}
